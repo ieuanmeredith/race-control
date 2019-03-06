b7523cd8d47fc91b68a6d6747edb4dbe2c4c0b7a
Ieuan Meredith <ieuanmeredith@googlemail.com>
2019-03-06 22:23:25 +0900

    gitignore

---
+++ src/_prototyping/nodesdk/test-transmit.ts
@@ -0,0 +1,44 @@
+const irsdk: any = require("node-irsdk");
+const fs: any = require("fs");
+
+import * as io from "socket.io-client";
+
+const socket: SocketIOClient.Socket = io("http://localhost:3000/receiver");
+
+socket.on("connect", () => {
+  if(socket.connected) {
+    console.log("Connected to the Race Control server");
+  } else {
+    console.log("Failed to connect to Race Control server");
+  }
+});
+
+irsdk.init({
+  telemetryUpdateInterval: 0,
+  sessionInfoUpdateInterval: 2000
+});
+
+const iracing: any = irsdk.getInstance();
+
+console.log("waiting for iRacing...");
+
+iracing.on("Connected", function (): void {
+  console.log("connected to iRacing..");
+});
+
+iracing.on("Disconnected", function (): void {
+  console.log("iRacing shut down, exiting.\n");
+  process.exit();
+});
+
+iracing.on("TelemetryDescription", function (data: any): void {
+  console.log("got TelemetryDescription");
+});
+
+iracing.on("Telemetry", function (data: any): void {
+  socket.emit("telemetry", data);
+});
+
+iracing.on("SessionInfo", function (data: any): void {
+  console.log("got SessionInfo");
+});
