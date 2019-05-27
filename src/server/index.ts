fc6adb3885b217635437874563779507f8bc5d5d
Ieuan <ieuan@onda.me>
2019-05-27 15:07:12 +0900

    moved timing logic to server for a universal state

--- src/server/index.ts
+++ src/server/index.ts
@@ -13,12 +13,29 @@ app.get("/", function(req: any, res: any): void {
 
 let sessionData = {};
 
+let drivers: any[] = [];
+let timingObjects: any[] = [];
+
+// lap time array definitions
+let carIdxCurrentLap: number[] = [];
+let carIdxCurrentLapStartTime: number[] = [];
+let carIdxLapTimes: string[][] = [];
+
+let carIdxPittedLap: number[] = [];
+let carIdxPittedStart: number[] = [];
+let carIdxPitTime: number[] = [];
+let carIdxPitLapRecord: number[][] = [];
+let carIdxPitLastStopTime: number[] = [];
+
+let carIdxStintRecord: number[][] = [];
+
 const receiver: SocketIO.Namespace =
   io.of("receiver")
   .on("connection", (socket: any) => {
     console.log("a transmitter connected");
 
     socket.on("telemetry", (data: ITelemetry) => {
+      // process and send telemetry feed
       // use custom DTO instead of 'data'
       // to minimize payload size
       const dto: Dto = new Dto();
@@ -39,25 +56,30 @@ const receiver: SocketIO.Namespace =
       dto.values.SessionTimeRemain = data.values.SessionTimeRemain;
       io.of("web").emit("telemetry_message", dto);
 
-      // timing
-      const timingDto: any = {
-        values: {}
-      };
-      timingDto.values.SessionTimeRemain = data.values.SessionTimeRemain;
-      timingDto.values.CarIdxClassPosition = data.values.CarIdxClassPosition;
-      timingDto.values.CarIdxEstTime = data.values.CarIdxEstTime;
-      timingDto.values.CarIdxF2Time = data.values.CarIdxF2Time;
-      timingDto.values.CarIdxLap = data.values.CarIdxLap;
-      timingDto.values.CarIdxLapCompleted = data.values.CarIdxLapCompleted;
-      timingDto.values.CarIdxLapDistPct = data.values.CarIdxLapDistPct;
-      timingDto.values.CarIdxOnPitRoad = data.values.CarIdxOnPitRoad;
-      timingDto.values.CarIdxPosition = data.values.CarIdxPosition;
-      io.of("web").emit("timing_message", timingDto);
+      // process and send timing info
+      if (drivers.length > 0) {
+        // process each acive in session (non-spectating/non-dc) driver
+        for (let i = 0; i < drivers.length; i++) {
+          if (drivers[i].CarIsPaceCar === 0
+            && drivers[i].IsSpectator === 0
+            && data.values.CarIdxPosition[i] > 0) {
+            processLapChange(data, i);
+            processPitlane(data, i);
+            populateTimingObject(data, i);
+          }
+        }
+        timingObjects = timingObjects.sort(
+          (a, b) => (a.Position > b.Position) ? 1 : ((b.Position > a.Position) ? -1 : 0)
+        );
+      }
+
+      io.of("web").emit("timing_message", timingObjects);
     });
 
     socket.on("session", (data) => {
       sessionData = data;
-      io.of("web").emit("session_message", data);
+      drivers = data.data.DriverInfo.Drivers;
+      // io.of("web").emit("session_message", data);
     });
 });
 
@@ -65,10 +87,102 @@ const web: SocketIO.Namespace =
   io.of("web")
   .on("connection", (socket: any) => {
     console.log("a user connected");
-    // send stored sessionData
-    io.of("web").emit("session_message", sessionData);
 });
 
 http.listen(3000, function(): void {
   console.log("listening on *:3000");
 });
+
+//#region private methods
+const populateTimingObject = (data: any, i: number) => {
+  timingObjects.push({
+    "Name": drivers[i].TeamName,
+    "DriverName": drivers[i].UserName,
+    "CarNum": drivers[i].CarNumber,
+    "Position": data.values.CarIdxPosition[i],
+    "ClassPosition": data.values.CarIdxClassPosition[i],
+    "OnPitRoad": data.values.CarIdxOnPitRoad[i],
+    "ClassColour": "#" + drivers[i].CarClassColor.toString(16),
+    "IRating": drivers[i].IRating,
+    "LicString": drivers[i].LicString,
+    "LicColor": "#" + drivers[i].LicColor.toString(16),
+    "EstTime": data.values.CarIdxEstTime[i].toFixed(1),
+    "PitTime": carIdxPitTime[i].toFixed(1),
+    "PitLastTime": carIdxPitLastStopTime[i] ? carIdxPitLastStopTime[i].toFixed(1) : 0,
+    "PittedLap": carIdxPittedLap[i],
+    "CarLap": data.values.CarIdxLap[i],
+    "StintLength": carIdxStintRecord[i][carIdxStintRecord[i].length - 1],
+    "LastLap": carIdxLapTimes[i][carIdxLapTimes[i].length - 1],
+  });
+};
+
+const processLapChange = (data: any, i: number) => {
+  if (!carIdxLapTimes[i]) {
+    carIdxLapTimes[i] = [];
+  }
+  if (carIdxCurrentLap[i] === -1) {
+    return;
+  }
+  // if telemetry lap is different to lap in memory
+  // new lap started, calculate and insert lap time to array
+  if (carIdxCurrentLap[i] !== data.values.CarIdxLap[i]) {
+    // calculate lap time
+    const sessionTime = data.values.SessionTime;
+    const startTime = carIdxCurrentLapStartTime[i];
+    if (!startTime) {
+      carIdxCurrentLapStartTime[i] = sessionTime;
+      carIdxCurrentLap[i] = data.values.CarIdxLap[i];
+      return;
+    }
+    if (!carIdxLapTimes[i]) {
+      carIdxLapTimes[i] = [];
+    }
+    const lapTimeSecs = (sessionTime - startTime);
+    const mins = lapTimeSecs / 60;
+    const seconds = lapTimeSecs - (60 * mins);
+    carIdxLapTimes[i].push(`${mins.toFixed(0)}:${seconds.toFixed(2)}`);
+    // insert lap time to array for carIdX
+    carIdxCurrentLapStartTime[i] = sessionTime;
+    // update current lap to new value
+    carIdxCurrentLap[i] = data.values.CarIdxLap[i];
+  }
+};
+
+const processPitlane = (data: any, i: number) => {
+  if (!carIdxPitLapRecord[i]) {
+    carIdxPitLapRecord[i] = [];
+  }
+  if (!carIdxStintRecord[i]) {
+    carIdxStintRecord[i] = [];
+  }
+  if (!carIdxPitTime[i]) {
+    carIdxPitTime[i] = 0;
+  }
+  // if car is on pit road and counter is 0
+  // car has just entered the pits
+  if (data.values.CarIdxOnPitRoad[i] && carIdxPitTime[i] === 0) {
+    carIdxStintRecord[i].push(data.values.CarIdxLap[i] -
+      carIdxPitLapRecord[i][carIdxPitLapRecord[i].length - 1]);
+    carIdxPittedStart[i] = data.values.SessionTime;
+    carIdxPittedLap[i] = data.values.CarIdxLap[i];
+    carIdxPitLapRecord[i].push(data.values.CarIdxLap[i]);
+    // set time in pits to non 0 value
+    carIdxPitTime[i] = 0.1;
+  }
+  // if car is on pit road and counter is > 0
+  // car is currently in the pits
+  else if (data.values.CarIdxOnPitRoad[i] && carIdxPitTime[i] > 0) {
+    const intermediate = (data.values.SessionTime) - (carIdxPittedStart[i]);
+    carIdxPitTime[i] =
+    intermediate > 0 ? intermediate : 0.1;
+  }
+  // if car is not on pit road
+  // set pit time to 0
+  else if (!data.values.CarIdxOnPitRoad[i]) {
+    if (carIdxPitTime[i] > 0 ) {
+      carIdxPitLastStopTime[i] = carIdxPitTime[i];
+      carIdxPitTime[i] = 0;
+    }
+  }
+};
+//#endregion
--- src/web/race-control-app/src/app/timing/timing.component.ts
+++ src/web/race-control-app/src/app/timing/timing.component.ts
@@ -8,21 +8,7 @@ import { SocketService } from "../socket.service";
 })
 
 export class TimingComponent implements OnInit {
-  public drivers: any[] = [];
   public timingObjects: any[] = [];
-
-  // lap time array definitions
-  public carIdxCurrentLap: number[] = [];
-  public carIdxCurrentLapStartTime: number[] = [];
-  public carIdxLapTimes: string[][] = [];
-
-  public carIdxPittedLap: number[] = [];
-  public carIdxPittedStart: number[] = [];
-  public carIdxPitTime: number[] = [];
-  public carIdxPitLapRecord: number[][] = [];
-  public carIdxPitLastStopTime: number[] = [];
-
-  public carIdxStintRecord: number[][] = [];
 
   constructor(private socketService: SocketService) {}
 
@@ -33,27 +19,8 @@ export class TimingComponent implements OnInit {
   private initSocketConnection(): void {
     this.socketService.initSocket();
 
-    // each event represents 1 tick of telemetry data
     this.socketService.onTelemetryMessage().subscribe((data: any) => {
-      if (this.drivers.length > 0) {
-        this.timingObjects = [];
-        // process each acive in session (non-spectating/non-dc) driver
-        for (let i = 0; i < this.drivers.length; i++) {
-          if (this.drivers[i].CarIsPaceCar === 0
-            && this.drivers[i].IsSpectator === 0
-            && data.values.CarIdxPosition[i] > 0) {
-            this.processLapChange(data, i);
-            this.processPitlane(data, i);
-            this.populateTimingObject(data, i);
-          }
-        }
-        this.timingObjects = this.timingObjects.sort((a, b) => (a.Position > b.Position) ? 1 : ((b.Position > a.Position) ? -1 : 0));
-      }
-    });
-
-    this.socketService.onSessionMessage().subscribe((data: any) => {
-      // update driver array to match server
-      this.drivers = data.data.DriverInfo.Drivers;
+      this.timingObjects = data;
     });
 
     this.socketService.onEvent("connect").subscribe(() => {
@@ -63,94 +30,4 @@ export class TimingComponent implements OnInit {
       console.log("disconnected from race control web server");
     });
   }
-
-    //#region private methods
-      private populateTimingObject(data: any, i: number) {
-        this.timingObjects.push({
-          "Name": this.drivers[i].TeamName,
-          "DriverName": this.drivers[i].UserName,
-          "CarNum": this.drivers[i].CarNumber,
-          "Position": data.values.CarIdxPosition[i],
-          "ClassPosition": data.values.CarIdxClassPosition[i],
-          "OnPitRoad": data.values.CarIdxOnPitRoad[i],
-          "ClassColour": "#" + this.drivers[i].CarClassColor.toString(16),
-          "IRating": this.drivers[i].IRating,
-          "LicString": this.drivers[i].LicString,
-          "LicColor": "#" + this.drivers[i].LicColor.toString(16),
-          "EstTime": data.values.CarIdxEstTime[i].toFixed(1),
-          "PitTime": this.carIdxPitTime[i].toFixed(1),
-          "PitLastTime": this.carIdxPitLastStopTime[i] ? this.carIdxPitLastStopTime[i].toFixed(1) : 0,
-          "PittedLap": this.carIdxPittedLap[i],
-          "CarLap": data.values.CarIdxLap[i],
-          "StintLength": this.carIdxStintRecord[i][this.carIdxStintRecord[i].length - 1],
-          "LastLap": this.carIdxLapTimes[i][this.carIdxLapTimes[i].length - 1]
-        });
-      }
-
-      private processLapChange(data: any, i: number) {
-        if (!this.carIdxLapTimes[i]) {
-          this.carIdxLapTimes[i] = [];
-        }
-        if (this.carIdxCurrentLap[i] === -1) {
-          return;
-        }
-        // if telemetry lap is different to lap in memory
-        // new lap started, calculate and insert lap time to array
-        if (this.carIdxCurrentLap[i] !== data.values.CarIdxLap[i]) {
-          // calculate lap time
-          const sessionTime = data.values.SessionTime;
-          const startTime = this.carIdxCurrentLapStartTime[i];
-          if (!startTime) {
-            this.carIdxCurrentLapStartTime[i] = sessionTime;
-            this.carIdxCurrentLap[i] = data.values.CarIdxLap[i];
-            return;
-          }
-          if (!this.carIdxLapTimes[i]) {
-            this.carIdxLapTimes[i] = [];
-          }
-          this.carIdxLapTimes[i].push((sessionTime - startTime).toFixed(2));
-          // insert lap time to array for carIdX
-          this.carIdxCurrentLapStartTime[i] = sessionTime;
-          // update current lap to new value
-          this.carIdxCurrentLap[i] = data.values.CarIdxLap[i];
-        }
-      }
-
-      private processPitlane(data: any, i: number) {
-        if (!this.carIdxPitLapRecord[i]) {
-          this.carIdxPitLapRecord[i] = [];
-        }
-        if (!this.carIdxStintRecord[i]) {
-          this.carIdxStintRecord[i] = [];
-        }
-        if (!this.carIdxPitTime[i]) {
-          this.carIdxPitTime[i] = 0;
-        }
-        // if car is on pit road and counter is 0
-        // car has just entered the pits
-        if (data.values.CarIdxOnPitRoad[i] && this.carIdxPitTime[i] === 0) {
-          this.carIdxPittedStart[i] = data.values.SessionTime;
-          this.carIdxPittedLap[i] = data.values.CarIdxLap[i];
-          this.carIdxPitLapRecord[i].push(data.values.CarIdxLap[i]);
-          // set time in pits to non 0 value
-          this.carIdxPitTime[i] = 0.01;
-        }
-        // if car is on pit road and counter is > 0
-        // car is currently in the pits
-        else if (data.values.CarIdxOnPitRoad[i] && this.carIdxPitTime[i] > 0) {
-          const intermediate = (data.values.SessionTime) - (this.carIdxPittedStart[i]);
-          this.carIdxPitTime[i] =
-          intermediate > 0 ? intermediate : 0.1;
-        }
-        // if car is not on pit road
-        // set pit time to 0
-        else if (!data.values.CarIdxOnPitRoad[i]) {
-          if (this.carIdxPitTime[i] > 0 ) {
-            this.carIdxStintRecord[i].push(data.values.CarIdxLap[i] - this.carIdxPitLapRecord[i][this.carIdxPitLapRecord[i].length - 1]);
-            this.carIdxPitLastStopTime[i] = this.carIdxPitTime[i];
-            this.carIdxPitTime[i] = 0;
-          }
-        }
-      }
-    //#endregion
 }
