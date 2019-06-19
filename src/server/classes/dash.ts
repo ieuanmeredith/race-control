befbda6cd103b627eb7970bd4329ede01faa2930
Ieuan <ieuan@onda.me>
2019-06-19 16:43:39 +0900

    refactoring dash telemetry

---
+++ src/server/classes/dash.ts
@@ -0,0 +1,138 @@
+import { Dto } from "./dto";
+
+export class Dash {
+  public Soc: number = 0;
+  public Deploy: number = 0;
+  public Flags: string[] = [];
+  public DeltaToSesBest: string = "+0.00";
+  public TimeLeft: string = "00:00:00";
+  public TrackTemp: string = "N/A";
+  public DeployMode: string = "0";
+  public Lap: number = 0;
+  public LapTimeArray: number[] = [];
+
+  public BufferLength: number = 18000;
+
+  public EstLapTime: number = 0;
+  public MaxFuel: number = 0;
+  public FuelUsageBuffer: number[] = [];
+  public FuelLapsRemaining: number = 0;
+  public FuelPerLap: string | number = 0;
+  public FuelRemaining: string | number = 0;
+  public Boxboxbox: boolean = false;
+
+  public Rpm: number = 0;
+  public FuelWeightRatio: number = 0.75;
+
+  public Gear: string = "N";
+
+  public GetDashDto(data: any): Dto {
+    this.Soc = Math.floor(data.values.EnergyERSBatteryPct * 100);
+    this.Deploy = Math.floor(data.values.EnergyMGU_KLapDeployPct * 100);
+    this.Flags = data.values.SessionFlags;
+    if (data.values.dcMGUKDeployFixed) {
+      this.DeployMode = data.values.dcMGUKDeployFixed.toString();
+    }
+    this.TrackTemp = data.values.TrackTempCrew.toFixed(2);
+    this.FuelRemaining = (Math.round(data.values.FuelLevel * 100) / 100).toFixed(2);
+
+    if (this.FuelUsageBuffer.length <= this.BufferLength) {
+      if (Math.floor(data.values.Speed) !== 0 && data.values.FuelLevel > 0.2) {
+        this.FuelUsageBuffer.push(Math.round(data.values.FuelUsePerHour * 100) / 100);
+      }
+    }
+    else {
+      this.FuelUsageBuffer = this.FuelUsageBuffer.splice(1, (this.BufferLength - 1));
+      this.FuelUsageBuffer.push(Math.round(data.values.FuelUsePerHour * 100) / 100);
+    }
+
+    if (this.Lap !== data.values.Lap) {
+      if (this.Lap === 0) {
+        this.Lap = data.values.Lap;
+      }
+      else {
+        this.Lap = data.values.Lap;
+        const lapTemp = Math.round(data.values.LapLastLapTime * 100) / 100;
+        if (lapTemp > 0 && this.LapTimeArray.indexOf(lapTemp) === -1) {
+          this.LapTimeArray.push(lapTemp);
+        }
+
+        if (this.LapTimeArray.length > 2) {
+          this.EstLapTime = this.GetAvgLap();
+        }
+
+        this.FuelLapsRemaining < 2 ? this.Boxboxbox = true : this.Boxboxbox = false;
+      }
+    }
+
+    if (this.MaxFuel > 0 && this.EstLapTime > 0) {
+      const lapsPerHour = 3600 / this.EstLapTime;
+      const fuelPerHour = this.GetAvgFuelPerHour();
+      const localFuelPerLap = fuelPerHour / lapsPerHour;
+      // minus 0.2L in kg to exclude last 0.2l from calculations
+      this.FuelLapsRemaining = (((data.values.FuelLevel * this.FuelWeightRatio) - (0.2 * this.FuelWeightRatio)) / localFuelPerLap);
+      this.FuelPerLap = (data.values.FuelLevel / this.FuelLapsRemaining).toFixed(2);
+      if (this.FuelLapsRemaining > 2) { this.Boxboxbox = false; }
+    }
+
+    const delta = data.values.LapDeltaToSessionBestLap.toFixed(2);
+    this.DeltaToSesBest = `${Number(delta) > 0 ? "+" : ""}${delta}`;
+
+    const secondsLeft = Math.floor(data.values.SessionTimeRemain);
+    const hours = Math.floor(secondsLeft / 3600);
+    const minutes = Math.floor((secondsLeft / 60) - hours * 60);
+    const seconds = secondsLeft - Math.floor((secondsLeft / 60)) * 60;
+    this.TimeLeft = `${this.Pad(hours.toString(), 2, 0)}:${this.Pad(minutes.toString(), 2, 0)}:${this.Pad(seconds.toString(), 2, 0)}`;
+    this.Rpm = data.values.RPM;
+    this.Gear = data.values.Gear === 0 ? "N"
+      : data.values.Gear === -1 ? "R"
+        : data.values.Gear.toString();
+
+    return this.GetDto(data);
+  }
+
+  private GetDto(data: any): Dto {
+    const dto: Dto = new Dto();
+    dto.values.Throttle = data.values.Throttle;
+    dto.values.Brake = data.values.Brake;
+    dto.values.SoC = this.Soc.toString();
+    dto.values.Deploy = this.Deploy.toString();
+    dto.values.FuelLevel = data.values.FuelLevel.toFixed(2);
+    dto.values.FuelLapsLeft = this.FuelLapsRemaining.toFixed(2);
+    dto.values.FuelPerLap = this.FuelPerLap.toString();
+    dto.values.Delta = this.DeltaToSesBest;
+    dto.values.BoxBoxBox = this.Boxboxbox;
+    dto.values.Flags = this.Flags;
+    dto.values.Gear = this.Gear;
+    dto.values.Temp = this.TrackTemp;
+    dto.values.SessionTimeRemain = this.TimeLeft;
+    dto.values.DeployMode = this.DeployMode;
+    dto.values.Speed = (data.values.Speed * 3.6).toFixed(0) + " kph ";
+    // convert input to useful value for animating rotation
+    dto.values.SteeringWheelAngle = ((data.values.SteeringWheelAngle * 180) / 3.14) * -1;
+
+    return dto;
+  }
+
+  private Pad(n: string, width: number, z: any) {
+    z = z || "0";
+    n = n + "";
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  }
+
+  private GetAvgLap(): number {
+    let sum = 0;
+    for (let i = 0; i < this.LapTimeArray.length; i++) {
+      sum += this.LapTimeArray[i];
+    }
+    return sum / this.LapTimeArray.length;
+  }
+
+  private GetAvgFuelPerHour(): number {
+    let sum = 0;
+    for (let i = 0; i < this.FuelUsageBuffer.length; i++) {
+      sum += this.FuelUsageBuffer[i];
+    }
+    return sum / this.FuelUsageBuffer.length;
+  }
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import { ITelemetry } from "./classes/telemetry";
 import { Dto } from "./classes/dto";
 import { Timing } from "./classes/timing";
+import { Dash } from "./classes/dash";
 
 // get port number from cmd arguments
 const port = process.argv[2];
@@ -17,143 +18,7 @@ app.get("/", function (req: any, res: any): void {
 let sessions: any[] = [];
 
 const timing = new Timing();
-
-//#region dash state variables
-let soc: number = 0;
-let deploy: number = 0;
-let flags: string[] = [];
-let deltaToSesBest: string = "+0.00";
-let timeLeft: string = "00:00:00";
-let trackTemp: string = "N/A";
-let deployMode: string = "0";
-let lap: number = 0;
-let lapTimeArray: number[] = [];
-
-let bufferLength: number = 18000;
-
-let estLapTime: number = 0;
-let maxFuel: number = 0;
-let fuelUsageBuffer: number[] = [];
-let fuelLapsRemaining: number = 0;
-let fuelPerLap: string | number = 0;
-let fuelRemaining: string | number = 0;
-let boxboxbox: boolean = false;
-
-let rpm: number = 0;
-let fuelWeightRatio: number = 0.75;
-
-let gear: string = "N";
-//#endregion
-
-//#region private methods
-const pad = (n: string, width: number, z: any) => {
-  z = z || "0";
-  n = n + "";
-  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-};
-
-const getAvgLap = (): number => {
-  let sum = 0;
-  for (let i = 0; i < lapTimeArray.length; i++) {
-    sum += lapTimeArray[i];
-  }
-  return sum / lapTimeArray.length;
-};
-
-const getAvgFuelPerHour = (): number => {
-  let sum = 0;
-  for (let i = 0; i < fuelUsageBuffer.length; i++) {
-    sum += fuelUsageBuffer[i];
-  }
-  return sum / fuelUsageBuffer.length;
-};
-
-// processes dash data and emits event to websocket
-const processDash = (data: any) => {
-  //#region process dash data
-  soc = Math.floor(data.values.EnergyERSBatteryPct * 100);
-  deploy = Math.floor(data.values.EnergyMGU_KLapDeployPct * 100);
-  flags = data.values.SessionFlags;
-  if (data.values.dcMGUKDeployFixed) {
-    deployMode = data.values.dcMGUKDeployFixed.toString();
-  }
-  trackTemp = data.values.TrackTempCrew.toFixed(2);
-  fuelRemaining = (Math.round(data.values.FuelLevel * 100) / 100).toFixed(2);
-
-  if (fuelUsageBuffer.length <= bufferLength) {
-    if (Math.floor(data.values.Speed) !== 0 && data.values.FuelLevel > 0.2) {
-      fuelUsageBuffer.push(Math.round(data.values.FuelUsePerHour * 100) / 100);
-    }
-  }
-  else {
-    fuelUsageBuffer = fuelUsageBuffer.splice(1, (bufferLength - 1));
-    fuelUsageBuffer.push(Math.round(data.values.FuelUsePerHour * 100) / 100);
-  }
-
-  if (lap !== data.values.Lap) {
-    if (lap === 0) {
-      lap = data.values.Lap;
-    }
-    else {
-      lap = data.values.Lap;
-      const lapTemp = Math.round(data.values.LapLastLapTime * 100) / 100;
-      if (lapTemp > 0 && lapTimeArray.indexOf(lapTemp) === -1) {
-        lapTimeArray.push(lapTemp);
-      }
-
-      if (lapTimeArray.length > 2) {
-        estLapTime = getAvgLap();
-      }
-
-      fuelLapsRemaining < 2 ? boxboxbox = true : boxboxbox = false;
-    }
-    const dto: Dto = new Dto();
-    dto.values.Throttle = data.values.Throttle;
-    dto.values.Brake = data.values.Brake;
-    dto.values.SoC = soc.toString();
-    dto.values.Deploy = deploy.toString();
-    dto.values.FuelLevel = data.values.FuelLevel.toFixed(2);
-    dto.values.FuelLapsLeft = fuelLapsRemaining.toFixed(2);
-    dto.values.FuelPerLap = fuelPerLap.toString();
-    dto.values.Delta = deltaToSesBest;
-    dto.values.BoxBoxBox = boxboxbox;
-    dto.values.Flags = flags;
-    dto.values.Gear = gear;
-    dto.values.Temp = trackTemp;
-    dto.values.SessionTimeRemain = timeLeft;
-    dto.values.DeployMode = deployMode;
-    dto.values.Speed = (data.values.Speed * 3.6).toFixed(0) + " kph ";
-    // convert input to useful value for animating rotation
-    dto.values.SteeringWheelAngle = ((data.values.SteeringWheelAngle * 180) / 3.14) * -1;
-    io.of("web").emit("telemetry_message", dto);
-  }
-
-  if (maxFuel > 0 && estLapTime > 0) {
-    const lapsPerHour = 3600 / estLapTime;
-    const fuelPerHour = getAvgFuelPerHour();
-    const localFuelPerLap = fuelPerHour / lapsPerHour;
-    // minus 0.2L in kg to exclude last 0.2l from calculations
-    fuelLapsRemaining = (((data.values.FuelLevel * fuelWeightRatio) - (0.2 * fuelWeightRatio)) / localFuelPerLap);
-    fuelPerLap = (data.values.FuelLevel / fuelLapsRemaining).toFixed(2);
-    if (fuelLapsRemaining > 2) { boxboxbox = false; }
-  }
-
-  const delta = data.values.LapDeltaToSessionBestLap.toFixed(2);
-  deltaToSesBest = `${Number(delta) > 0 ? "+" : ""}${delta}`;
-
-  const secondsLeft = Math.floor(data.values.SessionTimeRemain);
-  const hours = Math.floor(secondsLeft / 3600);
-  const minutes = Math.floor((secondsLeft / 60) - hours * 60);
-  const seconds = secondsLeft - Math.floor((secondsLeft / 60)) * 60;
-  timeLeft = `${pad(hours.toString(), 2, 0)}:${pad(minutes.toString(), 2, 0)}:${pad(seconds.toString(), 2, 0)}`;
-  rpm = data.values.RPM;
-  gear = data.values.Gear === 0 ? "N"
-    : data.values.Gear === -1 ? "R"
-      : data.values.Gear.toString();
-  //#endregion
-};
-
-//#endregion
+const dash = new Dash();
 
 //#region socket methods
 const web: SocketIO.Namespace =
@@ -181,8 +46,7 @@ const receiver: SocketIO.Namespace =
         const data: ITelemetry = msg.data;
         timing.CheckSessionState(data);
 
-        processDash(data);
-
+        io.of("web").emit("telemetry_message", dash.GetDashDto(data));
         io.of("web").emit("timing_message", timing.GetTimingObjArray(data));
       }
     });
@@ -208,12 +72,12 @@ const receiver: SocketIO.Namespace =
           timing.Drivers = session.data.DriverInfo.Drivers;
         }
         //#region taking session values for dash
-        if (fuelWeightRatio !== session.data.DriverInfo.DriverCarFuelKgPerLtr) {
-          fuelWeightRatio = session.data.DriverInfo.DriverCarFuelKgPerLtr;
+        if (dash.FuelWeightRatio !== session.data.DriverInfo.DriverCarFuelKgPerLtr) {
+          dash.FuelWeightRatio = session.data.DriverInfo.DriverCarFuelKgPerLtr;
         }
-        maxFuel = session.data.DriverInfo.DriverCarFuelMaxLtr * fuelWeightRatio;
+        dash.MaxFuel = session.data.DriverInfo.DriverCarFuelMaxLtr * dash.FuelWeightRatio;
 
-        if (estLapTime === 0) { estLapTime = session.data.DriverInfo.DriverCarEstLapTime; }
+        if (dash.EstLapTime === 0) { dash.EstLapTime = session.data.DriverInfo.DriverCarEstLapTime; }
         //#endregion
       }
     });
